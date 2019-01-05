138413ca7f84296042f4f9d844459e12367fedbc
taiyab12 <taiyabadil143@gmail.com.com>
2019-01-05 10:57:35 +0530

    jsdoc  first commit

---
+++ calculator/calculator.js
@@ -0,0 +1,14 @@
+/**
+ * 1. npm install -g jsdoc
+ * 2. sudo npm i -g jsdoc // if above not work
+ * 3. jsdoc filename.js   // to run this file and generate doc file
+ *    // you can see it in out global html
+ * 
+ * This function takes two params and returns the sum
+ * @param {number} a -The first param
+ * @param {number} b - The second param
+ * @returns {number} -result - The sum of two number
+ */
+function sum(a,b){
+    return a+b;
+}
